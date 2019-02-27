807d5e2e1f78fbd7e74a17dc13b0d0c1afaf75a6
Zach Calvert <zach@cloudbolt.io>
2019-02-27 09:55:12 -0800

    wire up song detail url, template and view

--- potify/music/urls.py
+++ potify/music/urls.py
@@ -1,20 +1,14 @@
 from django.urls import path
-from music.views import SongList
-from music.views import SongDetail
-from music.views import ArtistList
-from music.views import ArtistDetail
-from music.views import AlbumList
-from music.views import AlbumDetail
-from music.views import PlaylistList
-from music.views import PlaylistDetail
+
+from music import views
 
 urlpatterns = [
-    path('songs/', SongList.as_view(), name='song_list'),
-    path('', SongDetail.as_view(), name='song_detail'),
-    path('artists/', ArtistList.as_view(), name='artist_list'),
-    #path('<slug:slug>/', ArtistDetail.as_view(), name='artist_detail'),
-    path('albums/', AlbumList.as_view(), name='album_list'),
-    #path('<slug:slug>/', AlbumDetail.as_view(), name='album_detail'),
-    path('playlists/', PlaylistList.as_view(), name='playlist_list'),
-    #path('<slug:slug>/', PlaylistDetail.as_view(), name='playlist_detail'),
+    path('songs/', views.SongList.as_view(), name='song_list'),
+    path('songs/<int:pk>', views.SongDetail.as_view(), name='song_detail'),
+    path('artists/', views.ArtistList.as_view(), name='artist_list'),
+    path('artists/<int:pk>', views.ArtistDetail.as_view(), name='artist_detail'),
+    path('albums/', views.AlbumList.as_view(), name='album_list'),
+    path('albums/<int:pk>', views.AlbumDetail.as_view(), name='album_detail'),
+    path('playlists/', views.PlaylistList.as_view(), name='playlist_list'),
+    path('playlists/<int:pk>', views.PlaylistDetail.as_view(), name='playlist_detail'),
 ]
