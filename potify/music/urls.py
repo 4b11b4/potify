0fb3236da2de43c7b60a69b332be2c1b72c4dd17
AB <4b11b4@gmail.com>
2019-02-26 17:40:07 -0800

    add artists, albums, playlist listviews

--- potify/music/urls.py
+++ potify/music/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from music.views import SongList
+from music.views import ArtistList
+from music.views import AlbumList
+from music.views import PlaylistList
 
 urlpatterns = [
     path('songs/', SongList.as_view(), name='song_list'),
+    path('artists/', ArtistList.as_view(), name='artist_list'),
+    path('albums/', AlbumList.as_view(), name='album_list'),
+    path('playlists/', PlaylistList.as_view(), name='playlist_list'),
 ]
--- potify/music/views.py
+++ potify/music/views.py
@@ -3,11 +3,27 @@ from django.views.generic import ListView
 from django.views.generic.base import TemplateView
 
 from .models import Song
+from .models import Artist 
+from .models import Album 
+from .models import Playlist 
 
 # Create your views here.
 
 class HomePageView(TemplateView):
     template_name = "base.html"
 
+
 class SongList(ListView):
     model = Song
+
+
+class ArtistList(ListView):
+    model = Artist
+
+
+class AlbumList(ListView):
+    model = Album 
+
+
+class PlaylistList(ListView):
+    model = Playlist 
