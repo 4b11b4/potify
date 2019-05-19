74f7aeb9003874abcd41550c83a50fe91ed2940d
AB <4b11b4@gmail.com>
2019-05-19 13:11:58 -0700

    setup restframework. add serializers, viewsets, urls

---
+++ potify/music/serializers.py
@@ -0,0 +1,25 @@
+from .models import Album, Artist, Song, Playlist
+from rest_framework import serializers
+
+
+class AlbumSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Album
+        exclude = ()
+
+
+class ArtistSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Artist
+        exclude = ()
+
+class SongSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Song
+        exclude = ()
+
+
+class PlaylistSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Playlist
+        exclude = ()
--- potify/music/views.py
+++ potify/music/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from django.utils import timezone
 from django.views.generic import ListView, DetailView
 from django.views.generic.base import TemplateView
+from rest_framework import viewsets
 
 from .models import Song, Artist, Album, Playlist
+from .serializers import SongSerializer, ArtistSerializer, AlbumSerializer
+from .serializers import PlaylistSerializer
 
 
 class HomePageView(TemplateView):
@@ -23,6 +26,11 @@ class SongDetail(DetailView):
         return context
 
 
+class SongViewSet(viewsets.ModelViewSet):
+    queryset = Song.objects.all()
+    serializer_class = SongSerializer
+
+
 class ArtistList(ListView):
     model = Artist
 
@@ -31,19 +39,32 @@ class ArtistDetail(DetailView):
     model = Artist
 
 
+class ArtistViewSet(viewsets.ModelViewSet):
+    queryset = Artist.objects.all()
+    serializer_class = ArtistSerializer
+
+
 class AlbumList(ListView):
     model = Album
 
 
 class AlbumDetail(DetailView):
-    model = Album 
+    model = Album
+
+
+class AlbumViewSet(viewsets.ModelViewSet):
+    queryset = Album.objects.all()
+    serializer_class = AlbumSerializer
 
 
 class PlaylistList(ListView):
-    model = Playlist 
+    model = Playlist
 
 
 class PlaylistDetail(DetailView):
     model = Playlist
-    
 
+
+class PlaylistViewSet(viewsets.ModelViewSet):
+    queryset = Playlist.objects.all()
+    serializer_class = PlaylistSerializer
