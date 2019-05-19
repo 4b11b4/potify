1fb4824f76de632c0be86a9c374db0ddcb512f64
Zach Calvert <zach@cloudbolt.io>
2019-05-19 11:56:42 -0700

    Add home page, make nav bar horizontal
    Some small tweaks to make the site a little more usable. Understanding that the whole frontend may be swapped out eventually, this is a small effort to make it a little more easy on the eyes.

--- potify/potify/urls.py
+++ potify/potify/urls.py
@@ -4,11 +4,13 @@ potify URL Configuration
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+
 from music.views import HomePageView
+
 
 urlpatterns = [
     path('grappelli/', include('grappelli.urls')),
     path('admin/', admin.site.urls),
-    path('', HomePageView.as_view()),
+    path('', HomePageView.as_view(), name='home'),
     path('', include('music.urls')),
 ]
