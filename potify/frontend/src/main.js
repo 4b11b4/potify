45d5f75a0a46c6acb4f5f40b5dfcaf375de262dc
AB <4b11b4@gmail.com>
2019-07-15 14:51:30 -0700

    Add Bootstrap imports in Vue app entry point (main.js)

--- potify/frontend/src/main.js
+++ potify/frontend/src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import BootstrapVue from 'bootstrap-vue'
+Vue.use(BootstrapVue)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 /* Currently, axios is loaded directly by the Songs
  * component. Loading it here did not allow the
  * component to utilize it.
