9cbf56a217bd59b2a1d1e77244801a0d56ed27c7
AB <4b11b4@gmail.com>
2019-08-31 18:23:54 -0700

    Formatting

--- potify/frontend/src/components/PlayerComponent.vue
+++ potify/frontend/src/components/PlayerComponent.vue
@@ -1,57 +1,44 @@
 <template>
   <div>
-
     <audio controls ref="player">
-      <source v-bind:src="audio" type="audio/mp3">
-      Your browser does not support the audio element.
+    <source v-bind:src="audio" type="audio/mp3">
+    Your browser does not support the audio element.
     </audio>
     <p><i>Vibrations of...</i><br>
-       <b>{{title}}</b>
+    <b>{{title}}</b>
     </p>
-
   </div>
 </template>
 
 <script>
-export default {
-  data: function() {
-    return {
-      name: '',
-      artist: '',
-      album: '',
-      path: '',
-      current_time: 0,
-      total_time: 0
-    } 
-  },
-  
-  /* This is a workaround... creating a function that gets mounted?
-     Isn't there a specific watcher "lifecycle" function? */
-  /* An error gets logged to the console on startup
-     that the media player can't load unsupported text/html type.
-     I believe this is because the "song" variable is empty at startup? */
-  mounted:
-  /* The HTML player does not recognize when the source changes. */
-  function() {
-    this.$watch('audio', function() {
-      this.$refs.player.load()
-      this.$refs.player.play()
-    });
-    /* eslint-disable */
-    console.log("refs")
-    console.log(this.$refs)
-    /* eslint-enable */
-  },
-  computed: {
-    /* Consider renaming to URL or something else */
-    audio() {
-      return this.$store.state.audio;
+  export default {
+    /* This is a workaround... creating a function that gets mounted?
+       Isn't there a specific watcher "lifecycle" function? */
+    /* An error gets logged to the console on startup
+       that the media player can't load unsupported text/html type.
+       I believe this is because the "song" variable is empty at startup? */
+    mounted:
+    /* The HTML player does not recognize when the source changes. */
+    function() {
+      this.$watch('audio', function() {
+        this.$refs.player.load()
+        this.$refs.player.play()
+      });
+      /* eslint-disable */
+      console.log("refs")
+      console.log(this.$refs)
+      /* eslint-enable */
     },
-    title() {
-      return this.$store.state.title;
+    computed: {
+      /* Consider renaming to URL or something else */
+      audio() {
+        return this.$store.state.audio;
+      },
+      title() {
+        return this.$store.state.title;
+      }
     }
   }
-}
 </script>
 
 <style>
