07fb9af67afeb920205fa7c7f4d43647fa800027
AB <4b11b4@gmail.com>
2019-09-06 22:40:55 -0700

    Add Artists

---
+++ potify/frontend/src/components/ArtistRow.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <b-row>
+      <b-col>{{ artist.name }}</b-col>
+      <b-col><a v-bind:href="artist.image">
+          <img v-bind:src="artist.image">
+          </a></b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ArtistRow',
+  props: {
+    artist: Object
+  }
+}
+</script>
+
+<style>
+img {height: 1em;
+
+}
+</style>
--- potify/frontend/src/components/Artists.vue
+++ potify/frontend/src/components/Artists.vue
@@ -1,13 +1,46 @@
 <template>
   <div>
     <h2>Artists</h2>
-    <p>This is a list of artists.</p>
+    <b-container fluid>
+
+      <b-row>
+        <b-col>Artist</b-col>
+        <b-col>Image</b-col>
+      </b-row>
+
+      <div
+        v-for="(artist, index) in artist_list"
+        :key="index">
+          <artist-row v-bind:artist="artist"></artist-row>
+      </div>
+
+    </b-container>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import ArtistRow from './ArtistRow.vue'
+
 export default {
-  name: 'Artists'
+  name: 'Artists',
+  data: function() {
+    return {
+      artist_list: []
+    }
+  },
+  components: {
+    ArtistRow 
+  },
+  mounted() {
+    axios.get('api/artists').then((response) => {
+      this.artist_list = response.data.results
+      /* eslint-disable */
+      console.log('Artist List:')
+      console.log(this.artist_list)
+      /* eslint-enable */
+    })
+  }
 }
 </script>
 
